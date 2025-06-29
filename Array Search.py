bcf89c028ea5ff0d54afefdc826442497c8b7cc9
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2025-06-29 14:37:51 -0400

    Delete Array Search.py

--- Array Search.py
+++
@@ -1,9 +0,0 @@
-#User function Template for python3
-
-class Solution:
-    #Complete the below function
-    def search(self,arr, x):
-        for i in range(len(arr)):
-            if(arr[i] == x):
-                return i
-        return -1
