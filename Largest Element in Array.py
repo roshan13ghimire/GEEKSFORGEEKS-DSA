1e47d905c84acefeae9a5e9f31aea5a4b114243c
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2025-06-29 14:38:05 -0400

    Delete Largest Element in Array.py

--- Largest Element in Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def largest(self, arr):
-        max = 0
-        for i in range(len(arr)):
-            if max < arr[i]:
-                max = arr[i]
-        return max
-        
